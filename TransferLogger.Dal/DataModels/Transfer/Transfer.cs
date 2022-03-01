148803f02f4347ea56059f2e9be60f1c1c257fb0
bebersdocks <bebersdocks@protonmail.com>
2022-03-01 10:41:00 +0400

    Dal: use student's organization for transfer

--- TransferLogger.Dal/DataModels/Transfer/Transfer.cs
+++ TransferLogger.Dal/DataModels/Transfer/Transfer.cs
@@ -8,16 +8,12 @@ namespace TransferLogger.Dal.DataModels
     [Table]
     public class Transfer
     {
-        [PrimaryKey, Identity] public int       TransferId     { get; set; }
-        [Column, NotNull]      public int       OrganizationId { get; set; }
-        [Column, NotNull]      public int       StudentId      { get; set; }
-        [Column, NotNull]      public DateTime  CreatedDt      { get; set; }
-        [Column, Nullable]     public DateTime? SubmittedDt    { get; set; }
+        [PrimaryKey, Identity] public int       TransferId  { get; set; }
+        [Column, NotNull]      public int       StudentId   { get; set; }
+        [Column, NotNull]      public DateTime  CreatedDt   { get; set; }
+        [Column, Nullable]     public DateTime? SubmittedDt { get; set; }
 
         #region Associations 
-
-        [Association(ThisKey = nameof(OrganizationId), OtherKey = nameof(OrganizationId), Relationship = Relationship.OneToOne, CanBeNull = false)]
-        public Organization Organization { get; set; }
 
         [Association(ThisKey = nameof(StudentId), OtherKey = nameof(StudentId), Relationship = Relationship.OneToOne, CanBeNull = false)]
         public Student Student { get; set; }
