301b91459f8ea9bf672fc4d9361d984fa22257d6
bebersdocks <bebersdocks@protonmail.com>
2022-03-01 10:07:24 +0400

    BusinessLogic: initial add

---
+++ TransferLogger.BusinessLogic/AppSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+using TransferLogger.Dal;
+
+namespace TransferLogger.BusinessLogic
+{
+    public class AppSettings
+    {
+        public DbSettings DbSettings { get; set; }
+
+        public static AppSettings Read()
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "appsettings.json");
+
+            return FileUtils.ReadJson<AppSettings>(path);
+        }
+    }
+}
---
+++ TransferLogger.BusinessLogic/FileUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+using Newtonsoft.Json;
+
+namespace TransferLogger.BusinessLogic
+{
+    public static class FileUtils
+    {
+        public static T ReadJson<T>(string path)
+        {
+            using (StreamReader file = File.OpenText(path))
+            {
+                var json = file.ReadToEnd();
+
+                return JsonConvert.DeserializeObject<T>(json);
+            }
+        }
+    }
+}
