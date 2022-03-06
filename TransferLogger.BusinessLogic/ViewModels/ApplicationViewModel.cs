87ecb0d0f4b6ae004e7069a00036a6915ef6951e
bebersdocks <bebersdocks@protonmail.com>
2022-03-06 19:21:00 +0600

    BusinessLogic: add application view model

---
+++ TransferLogger.BusinessLogic/ViewModels/ApplicationViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TransferLogger.BusinessLogic.ViewModels
+{
+    public class ApplicationViewModel
+    {
+        public int    ApplicationId     { get; set; }
+        public string ApplicationStatus { get; set; }
+        public string Student           { get; set; }
+        public string Organization      { get; set; }
+        public int    CoursesCount      { get; set; }
+    }
+}
