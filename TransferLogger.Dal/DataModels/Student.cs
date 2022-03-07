9bb856fc3458f73a92fdc385098e78ceb9df83ce
bebersdocks <bebersdocks@protonmail.com>
2022-03-07 19:01:41 +0600

    Dal: changes

--- TransferLogger.Dal/DataModels/Student.cs
+++ TransferLogger.Dal/DataModels/Student.cs
@@ -5,19 +5,13 @@ namespace TransferLogger.Dal.DataModels
     [Table]
     public class Student
     {
-        [PrimaryKey, Identity] public int    StudentId      { get; set; }
-        [Column, NotNull]      public int    OrganizationId { get; set; }
-        [Column, Nullable]     public string Reference      { get; set; }
-        [Column, NotNull]      public string Name           { get; set; }
-        [Column, NotNull]      public string Surname        { get; set; }
-        [Column, Nullable]     public string Phone          { get; set; }
-        [Column, Nullable]     public string Email          { get; set; }
+        [PrimaryKey, Identity] public int    StudentId { get; set; }
+        [Column, Nullable]     public string Reference { get; set; }
+        [Column, NotNull]      public string Name      { get; set; }
+        [Column, NotNull]      public string Surname   { get; set; }
+        [Column, Nullable]     public string Phone     { get; set; }
+        [Column, Nullable]     public string Email     { get; set; }
 
-        #region Associations 
-
-        [Association(ThisKey = nameof(OrganizationId), OtherKey = nameof(OrganizationId), Relationship = Relationship.OneToOne, CanBeNull = false)]
-        public Organization Organization { get; set; }
-
-        #endregion
+        [NotColumn] public string DisplayString => $"{Name} {Surname}";
     }
 }
--- TransferLogger.Dal/DataModels/Transfer/Application.cs
+++ TransferLogger.Dal/DataModels/Transfer/Application.cs
@@ -17,6 +17,7 @@ namespace TransferLogger.Dal.DataModels.Transfer
         [PrimaryKey, Identity] public int               ApplicationId     { get; set; }
         [Column, NotNull]      public ApplicationStatus ApplicationStatus { get; set; }
         [Column, NotNull]      public int               StudentId         { get; set; }
+        [Column, NotNull]      public int               OrganizationId    { get; set; }
         [Column, NotNull]      public DateTime          CreatedAt         { get; set; }
         [Column, Nullable]     public DateTime?         SubmittedAt       { get; set; }
 
@@ -24,6 +25,12 @@ namespace TransferLogger.Dal.DataModels.Transfer
 
         [Association(ThisKey = nameof(StudentId), OtherKey = nameof(StudentId), Relationship = Relationship.OneToOne, CanBeNull = false)]
         public Student Student { get; set; }
+
+        [Association(ThisKey = nameof(OrganizationId), OtherKey = nameof(OrganizationId), Relationship = Relationship.OneToOne, CanBeNull = false)]
+        public Organization Organization { get; set; }
+
+        [Association(ThisKey = nameof(ApplicationId), OtherKey = nameof(ApplicationId), Relationship = Relationship.OneToMany)]
+        public IEnumerable<ApplicationCourse> Courses { get; set; }
 
         [Association(ThisKey = nameof(ApplicationId), OtherKey = nameof(ApplicationId), Relationship = Relationship.OneToMany)]
         public IEnumerable<ApplicationExcelLocation> ExcelLocations { get; set; }
