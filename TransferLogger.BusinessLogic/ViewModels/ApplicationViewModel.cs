4a048c70ef04076aa279afc10b41af539c6d58fe
bebersdocks <bebersdocks@protonmail.com>
2022-03-09 23:22:56 +0600

    BusinessLogic: changes

--- TransferLogger.BusinessLogic/ViewModels/ApplicationViewModel.cs
+++ TransferLogger.BusinessLogic/ViewModels/ApplicationViewModel.cs
@@ -1,11 +1,32 @@
-﻿namespace TransferLogger.BusinessLogic.ViewModels
+﻿using System;
+using System.Linq;
+
+using TransferLogger.Dal.DataModels;
+using TransferLogger.Dal.DataModels.Transfer;
+
+namespace TransferLogger.BusinessLogic.ViewModels
 {
     public class ApplicationViewModel
     {
-        public int    ApplicationId     { get; set; }
-        public string ApplicationStatus { get; set; }
-        public string Student           { get; set; }
-        public string Organization      { get; set; }
-        public int    CoursesCount      { get; set; }
+        public int       ApplicationId     { get; set; }
+        public string    ApplicationStatus { get; set; }
+        public string    Student           { get; set; }
+        public string    Organization      { get; set; }
+        public int       CoursesCount      { get; set; }
+        public DateTime  CreatedAt         { get; set; }
+        public DateTime? SubmittedAt       { get; set; }
+
+        public ApplicationViewModel() {}
+
+        public ApplicationViewModel(Application app, Student student, Organization org)
+        {
+            ApplicationId     = app.ApplicationId;
+            ApplicationStatus = app.ApplicationStatus.ToString();
+            Student           = student.DisplayString;
+            Organization      = org.Name;
+            CoursesCount      = app.Courses.Count();
+            CreatedAt         = app.CreatedAt;
+            SubmittedAt       = app.SubmittedAt;
+        }
     }
 }
