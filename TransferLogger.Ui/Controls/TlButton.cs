ec152cdb10565aa0f06bd4073aff6bda3b9291b8
bebersdocks <bebersdocks@protonmail.com>
2022-03-09 23:51:39 +0600

    Ui: set properties for tl controls in constructors

--- TransferLogger.Ui/Controls/TlButton.cs
+++ TransferLogger.Ui/Controls/TlButton.cs
@@ -2,14 +2,6 @@
 {
     public class TlButton : Button
     {
-        public override bool             AutoSize    => true;
-        public override Color            BackColor   => Color.FromArgb(250, 250, 255);
-        public override Cursor           Cursor      => Cursors.Hand;
-        public override Color            ForeColor   => Color.Black;
-        public override Size             MaximumSize => new(0, 30);
-        public override Size             MinimumSize => new(0, 30);
-        public override ContentAlignment TextAlign   => ContentAlignment.MiddleCenter;
-
         private Color _hoverColor = Color.FromArgb(234, 240, 255);
         public Color HoverColor
         {
@@ -17,18 +9,22 @@
             set { _hoverColor = value; Invalidate(); }
         }
 
-        protected override void WndProc(ref Message m)
+        public TlButton()
         {
-            base.WndProc(ref m);
-
-            FlatStyle = FlatStyle.Flat;
+            AutoSize    = true;
+            BackColor   = Color.FromArgb(250, 250, 255);
+            Cursor      = Cursors.Hand;
+            FlatStyle   = FlatStyle.Flat;
+            ForeColor   = Color.Black;
+            MaximumSize = new Size(0, 28);
+            MinimumSize = new Size(0, 28);
+            TabStop     = false;
+            TextAlign   = ContentAlignment.MiddleCenter;
 
             FlatAppearance.BorderColor        = Color.Gainsboro;
             FlatAppearance.BorderSize         = 1;
             FlatAppearance.MouseDownBackColor = BackColor;
             FlatAppearance.MouseOverBackColor = _hoverColor;
-
-            TabStop = false;
         }
     }
 }
---
+++ TransferLogger.Ui/Controls/TlTextBox.cs
@@ -0,0 +1,22 @@
+﻿namespace TransferLogger.Ui.Controls
+{
+    public class TlTextBox : TextBox
+    {
+        private int _boxWidth = 325;
+        public int BoxWidth
+        {
+            get { return _boxWidth; }
+            set { _boxWidth = value; Invalidate(); }
+        }
+
+        public TlTextBox()
+        {
+            Width = _boxWidth;
+            
+            BackColor   = Color.GhostWhite;
+            BorderStyle = BorderStyle.Fixed3D;
+            ForeColor   = Color.Black;
+            TextAlign   = HorizontalAlignment.Left;
+        }
+    }
+}
