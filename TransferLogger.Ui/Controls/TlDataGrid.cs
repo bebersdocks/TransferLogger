1b3c9b1d57110c27e84f7cbf1e247424c395ab0f
bebersdocks <bebersdocks@protonmail.com>
2022-03-10 16:15:37 +0600

    Ui: add data grid control

---
+++ TransferLogger.Ui/Controls/TlDataGrid.cs
@@ -0,0 +1,73 @@
+﻿namespace TransferLogger.Ui.Controls
+{
+    public class TlDataGrid : DataGridView
+    {
+        private static DataGridViewCellStyle GetColumnsHeaderDefaultStyle()
+        {
+            return new DataGridViewCellStyle
+            {
+                Alignment          = DataGridViewContentAlignment.MiddleCenter,
+                BackColor          = Color.AliceBlue,
+                Font               = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point),
+                ForeColor          = Color.Black,
+                Padding            = new Padding(0, 5, 0, 5),
+                SelectionBackColor = Color.AliceBlue,
+                SelectionForeColor = Color.Black,
+                WrapMode           = DataGridViewTriState.False
+            };
+        }
+
+        private static DataGridViewCellStyle GetDefaultCellStyle()
+        {
+            return new DataGridViewCellStyle
+            {
+                Alignment          = DataGridViewContentAlignment.MiddleCenter,
+                BackColor          = Color.White,
+                Font               = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point),
+                ForeColor          = Color.Black,
+                SelectionBackColor = Color.WhiteSmoke,
+                SelectionForeColor = Color.Black,
+                WrapMode           = DataGridViewTriState.False
+            };
+        } 
+
+        private static DataGridViewCellStyle GetRowHeadersDefaultCellStyle()
+        {
+            return new DataGridViewCellStyle
+            {
+                Alignment          = DataGridViewContentAlignment.MiddleLeft,
+                BackColor          = Color.White,
+                Font               = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point),
+                ForeColor          = SystemColors.WindowText,
+                SelectionBackColor = SystemColors.Highlight,
+                SelectionForeColor = SystemColors.HighlightText,
+                WrapMode           = DataGridViewTriState.True,
+            };
+        }
+
+        public TlDataGrid()
+        {
+            AllowUserToAddRows        = false;
+            AllowUserToDeleteRows     = false;
+            AllowUserToOrderColumns   = true;
+            Anchor                    = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            AutoSizeColumnsMode       = DataGridViewAutoSizeColumnsMode.Fill;
+            BackgroundColor           = Color.White;
+            BorderStyle               = BorderStyle.None;
+            CellBorderStyle           = DataGridViewCellBorderStyle.Raised;
+            Cursor                    = Cursors.Hand;     
+            EnableHeadersVisualStyles = false;
+            MultiSelect               = false;
+            ReadOnly                  = true;
+            RowHeadersVisible         = false;
+            RowTemplate.Height        = 25;
+            SelectionMode             = DataGridViewSelectionMode.FullRowSelect;
+            TabIndex                  = 0;
+
+            ColumnHeadersDefaultCellStyle = GetColumnsHeaderDefaultStyle();
+            ColumnHeadersHeightSizeMode   = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DefaultCellStyle              = GetDefaultCellStyle();
+            RowHeadersDefaultCellStyle    = GetRowHeadersDefaultCellStyle();
+        }
+    }
+}
