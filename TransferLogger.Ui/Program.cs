f3eab1c012188dcd2ac4e5b80f020a4581273d5f
bebersdocks <bebersdocks@protonmail.com>
2022-03-15 17:14:22 +0600

    Ui: add multi-target compilation

--- TransferLogger.Ui/Program.cs
+++ TransferLogger.Ui/Program.cs
@@ -12,7 +12,12 @@ namespace TransferLogger.Ui
         [STAThread]
         static void Main()
         {
+#if NET6_0_OR_GREATER
             ApplicationConfiguration.Initialize();
+#else
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+#endif
 
             var appSettings = AppSettings.Read();
 
