1ac8fbb6b9f4fc86143d253db71922d5a2414cfb
bebersdocks <bebersdocks@protonmail.com>
2022-03-15 17:22:23 +0600

    Dal: add support for windows authentication

--- TransferLogger.Dal/DbSettings.cs
+++ TransferLogger.Dal/DbSettings.cs
@@ -2,19 +2,31 @@
 {
     public class DbSettings
     {
-        public string Username     { get; set; }
-        public string Password     { get; set; }
-        public string Server       { get; set; }
-        public int    Port         { get; set; }
-        public string Database     { get; set; }
-        public string ProviderName { get; set; }
+        public string Username                 { get; set; }
+        public string Password                 { get; set; }
+        public string Server                   { get; set; }
+        public int    Port                     { get; set; }
+        public string Database                 { get; set; }
+        public string ProviderName             { get; set; }
+        public bool   UseWindowsAuthentication { get; set; }
 
         public override string ToString()
         {
             if (string.Equals(ProviderName, LinqToDB.ProviderName.SqlServer))
-                return $"User Id={Username};Password={Password};Server={Server};Database={Database};Connect Timeout=20";
+            {
+                if (UseWindowsAuthentication)
+                {
+                    return $"Integrated Security=SSPI;Server={Server};Database={Database};Connect Timeout=20";
+                }
+                else
+                {
+                    return $"User Id={Username};Password={Password};Server={Server};Database={Database};Connect Timeout=20";
+                }
+            }
             else
+            {
                 return $"User Id={Username};Password={Password};Server={Server};Port={Port};Database={Database};CommandTimeout=20";
+            }
         }
     }
 }
