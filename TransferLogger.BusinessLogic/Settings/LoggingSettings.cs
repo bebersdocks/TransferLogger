bb5a137d0ca6b96101f8b3810ca05a6ba93a4979
bebersdocks <bebersdocks@protonmail.com>
2022-04-25 15:31:59 +0600

    Ui: set default logs path to executing directory

--- TransferLogger.BusinessLogic/Settings/LoggingSettings.cs
+++ TransferLogger.BusinessLogic/Settings/LoggingSettings.cs
@@ -4,7 +4,7 @@ namespace TransferLogger.BusinessLogic.Settings
 {
     public class LoggingSettings
     {
-        public string        Path          { get; set; } = "../_logs";
+        public string        Path          { get; set; } = "_logs";
         public string        FileName      { get; set; } = "transfer_logger";
         public LogEventLevel Level         { get; set; } = LogEventLevel.Debug;
         public LogEventLevel OverrideLevel { get; set; } = LogEventLevel.Debug;
